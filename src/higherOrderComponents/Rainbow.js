5346ee5c2c6970a15377779aebf9a97a5e15303f
Wojciech Danowski <wojciechdanowskiz@gmail.com>
2019-12-17 22:35:11 +0100

    Implemented a return method wich takes props from spread opperator into our component

--- src/higherOrderComponents/Rainbow.js
+++ src/higherOrderComponents/Rainbow.js
@@ -4,4 +4,14 @@ const Rainbow = WrappedComponent => {
   const colours = ["blue", "pink", "orange", "red", "green", "yellow"];
   const randomColour = colours[Math.floor(Math.random() * 5)];
   const className = randomColour + "-text";
+
+  return props => {
+    return (
+      <div className={className}>
+        <WrappedComponent {...props} />
+      </div>
+    );
+  };
 };
+
+export default Rainbow;
